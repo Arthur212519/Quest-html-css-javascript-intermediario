2eae7855b8b58645ebabd4ae47f3c04d8892f9a9
Arthur Santos <arthurolmo.negociosonline@gmail.com>
2023-02-06 13:19:19 -0300

    javascript-update

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,52 +1,23 @@
 const enviar = document.getElementById("enviar")
-const nome = document.getElementById("nome")
-const email = document.getElementById("email")
-const tel = document.getElementById("tel")
-const mensagem = document.getElementById("mensagem")
 const erro =document.querySelectorAll(".campo-obrigatorio")
+const inputs =document.querySelectorAll(".input")
 
-enviar.addEventListener("click",function(){
-    const nomeValue =nome.value;
-    const emailValue =email.value;
-    const telValue =tel.value;
-    const mensagemValue =mensagem.value;
-    if(nomeValue!==""){
-        nome.classList.remove("campo-vermelho")
-        nome.classList.add("campo-verde")
-        erro[0].classList.add("invisivel")
-    }else{
-        nome.classList.remove("campo-verde")
-        nome.classList.add("campo-vermelho")
-        erro[0].classList.remove("invisivel")
-    }
-    if(emailValue!==""){
-        email.classList.remove("campo-vermelho")
-        email.classList.add("campo-verde")
-        erro[1].classList.add("invisivel")
-    }else{
-        email.classList.remove("campo-verde")
-        email.classList.add("campo-vermelho")
-        erro[1].classList.remove("invisivel")
-    }
-    if(telValue!==""){
-        tel.classList.remove("campo-vermelho")
-        tel.classList.add("campo-verde")
-        erro[2].classList.add("invisivel")
-    }else{
-        tel.classList.remove("campo-verde")
-        tel.classList.add("campo-vermelho")
-        erro[2].classList.remove("invisivel")
-    }
-    if(mensagemValue!==""){
-        mensagem.classList.remove("campo-vermelho")
-        mensagem.classList.add("campo-verde")
-        erro[3].classList.add("invisivel")
-    }else{
-        mensagem.classList.remove("campo-verde")
-        mensagem.classList.add("campo-vermelho")
-        erro[3].classList.remove("invisivel")
-    }
+enviar.addEventListener('click',()=>{
+    inputs.forEach((input,number)=>{
+        const value = input.value
+        if(value==''){
+            input.classList.remove('campo-verde')
+            input.classList.add('campo-vermelho')
+            erro[number].classList.remove('invisivel')
+
+        } else{
+            input.classList.remove('campo-vermelho')
+            input.classList.add('campo-verde') 
+            erro[number].classList.add('invisivel')
+        }
+    })
 })
+
 
 
 
